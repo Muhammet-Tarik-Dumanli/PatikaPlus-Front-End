45ef5665d7b9cf2662c9e6d0c2873c109d342955
Muhammet-Tarik-Dumanli <mtdumanli@gmail.com>
2025-05-28 16:00:25 +0300

    bug fix

--- Week7/js/main.js
+++ Week7/js/main.js
@@ -15,6 +15,10 @@
     selector: "#photos .photo",
   });
 
+  var glightbox = GLightbox({
+    selector: ".glightbox",
+  });
+
   var comment = new Swiper("#comments .swiper", {
     pagination: {
       el: ".swiper-pagination",
@@ -22,4 +26,56 @@
       type: "bullets",
     },
   });
+
+  var header = document.getElementById("header");
+  var headerScrolled = (event) => {
+    if (window.scrollY > 100) {
+      header.classList.add("header-scrolled");
+    } else {
+      header.classList.remove("header-scrolled");
+    }
+  };
+
+  window.addEventListener("load", headerScrolled);
+  document.addEventListener("scroll", headerScrolled);
+
+  var links = document.getElementsByClassName("scrollto");
+
+  var focusSectionLink = (event) => {
+    for (const link of links) {
+      var id = link.hash.slice(1);
+      var section = document.getElementById(id);
+      var position = window.scrollY + window.innerHeight / 2;
+
+      if (
+        position >= section.offsetTop &&
+        position <= section.offsetTop + section.offsetHeight
+      ) {
+        link.ariaCurrent = "page";
+        link.classList.add("active");
+      } else {
+        link.ariaCurrent = null;
+        link.classList.remove("active");
+      }
+    }
+  };
+
+  var focusSection = (event) => {
+    event.preventDefault();
+    var id = event.target.hash.slice(1);
+    var section = document.getElementById(id);
+
+    if (section) {
+      window.scrollTo({
+        top: section.offsetTop - header.offsetHeight,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  window.addEventListener("scroll", focusSectionLink);
+
+  for (const link of links) {
+    link.addEventListener("click", focusSection);
+  }
 })();
