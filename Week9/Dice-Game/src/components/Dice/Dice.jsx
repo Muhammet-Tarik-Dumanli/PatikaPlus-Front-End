a2ce05fca649597403dc1124f3fb1073b0545b0f
Muhammet Tarık DUMANLI <mtdumanli@gmail.com>
2025-06-17 14:05:33 +0300

    player score

--- Week9/Dice-Game/src/components/Dice/Dice.jsx
+++ Week9/Dice-Game/src/components/Dice/Dice.jsx
@@ -6,6 +6,8 @@ function Dice() {
     const [diceRight, setDiceRight] = useState(1);
     const [isRolling, setIsRolling] = useState(false);
     const [player1Name, setPlayer1Name] = useState('Player 1');
+    const [player1Score, setPlayer1Score] = useState(0);
+    const [player2Score, setPlayer2Score] = useState(0);
 
     const rollDice = () => {
         setIsRolling(true);
@@ -21,12 +23,18 @@ function Dice() {
             setDiceLeft(randomNumber1);
             setDiceRight(randomNumber2);
             setIsRolling(false);
-        }, 3000)
+
+            if (randomNumber1 > randomNumber2) {
+                setPlayer1Score(prev => prev + 1);
+            } else if (randomNumber2 > randomNumber1) {
+                setPlayer2Score(prev => prev + 1);
+            }
+        }, 3000);
 
     };
 
     const getWinnerMessage = () => {
-        if(isRolling) return 'Rolling...'
+        if (isRolling) return 'Rolling...'
         if (diceLeft > diceRight) return '🚩 Player 1 Wins!';
         if (diceRight > diceLeft) return 'Player 2 Wins! 🚩';
         return 'Draw!';
@@ -40,12 +48,14 @@ function Dice() {
             <div className="container">
                 <h1 className="winner">{getWinnerMessage()}</h1>
                 <div className="dice">
-                    <p><input type="text" className="input" value={player1Name} onChange={handleNameChange} placeholder="Player 1" disabled={isRolling}/></p>
+                    <p><input type="text" className="input" value={player1Name} onChange={handleNameChange} placeholder="Player 1" disabled={isRolling} /></p>
+                    <p className="score">Score: {player1Score}</p>
                     <img src={`./src/assets/images/dice${diceLeft}.png`} alt="leff-dice" className="img1" />
                 </div>
 
                 <div className="dice">
                     <p>Player 2</p>
+                    <p className="score">Score: {player2Score}</p>
                     <img src={`./src/assets/images/dice${diceRight}.png`} alt="right-dice" className="img2" />
                 </div>
             </div>
