a7573272111ce734c75c7e91c74f1f2534c3a528
Muhammet Tarık DUMANLI <mtdumanli@gmail.com>
2025-07-08 10:51:02 +0300

    adding header to app.jsx

--- Week12/ToDo-App/src/App.jsx
+++ Week12/ToDo-App/src/App.jsx
@@ -1,11 +1,24 @@
+import { useState } from 'react'
+import Header from './components/Header'
 import './App.css'
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addToDo = (text) => {
+    const trimmed = text.trim();
+
+    if (trimmed !== '') {
+      const newToDo = { text: trimmed, done: false };
+      setTodos([...todos, newToDo]);
+    }
+  }
 
   return (
-    <>
-      
-    </>
+    <section className='todoapp'>
+      <Header addToDo={addToDo}/>
+      <pre>{JSON.stringify(todos, null, 2)}</pre>
+    </section>
   )
 }
 
