6d00020e5fe644fbd9dd7456535c50a28591ac96
Muhammet Tarık DUMANLI <mtdumanli@gmail.com>
2025-07-08 16:35:00 +0300

    start coding

--- Week12/StarWars-App/src/App.jsx
+++ Week12/StarWars-App/src/App.jsx
@@ -1,11 +1,17 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css'
+import Home from './pages/Home';
+import StarshipDetail from './pages/StarshipDetail';
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/starship/:id' element={<StarshipDetail />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
